6b3654cee34fd6e3775e4c1d90c793160630011b
cherry larry <mycherrylarry@gmail.com>
2014-06-12 17:51:52 +0900

    added example for akka

--- msg-stream-server/src/main/scala/net/cherry/stream/application/Main.scala
+++ msg-stream-server/src/main/scala/net/cherry/stream/application/Main.scala
@@ -1,13 +1,50 @@
 package net.cherry.stream.application
 
-import net.cherry.infrastructure.uuid.UUID
+import akka.actor.{Props, ActorSystem, Actor}
 
+sealed trait Received
+
+case class ReceivedA(param1: String, param2: String) extends Received
+
+case class ReceivedB(param1: String) extends Received
+
+case class ReceivedC(param1: String, param2: String, param3: Int) extends Received
+
+case class Completed(msg: String)
+
+class MyActor1 extends Actor {
+  def receive = {
+    case ReceivedC(msg1, msg2, msg3) =>
+      sender ! Completed("myactor 1 completed")
+    case _ =>
+      println("unknown")
+  }
+}
+
+class MyActor extends Actor {
+  val myActor1 = context.actorOf(Props[MyActor1], "my_actor_1")
+  def receive = {
+    case ReceivedA(p1, p2) =>
+      println("received A: " + p1 + p2)
+    case ReceivedB(p) =>
+      println("received B: " + p)
+      myActor1 ! ReceivedC("msg1", "msg2", 0)
+    case Completed(msg) =>
+      println(s"completed: $msg")
+    case _ =>
+      println("unknown")
+  }
+}
 
 object Main extends App {
 
-  val uuid = UUID
-  val uuid2 = UUID
-  println("xxxx")
-  println(uuid.value)
-  println(uuid2.value)
+  val system = ActorSystem("mySystem")
+  val myActor = system.actorOf(Props[MyActor], "myactor")
+
+  myActor ! ReceivedA("ab", "cd")
+  while (true) {
+    Thread.sleep(1000)
+    myActor ! ReceivedB("abc")
+  }
 }
+
