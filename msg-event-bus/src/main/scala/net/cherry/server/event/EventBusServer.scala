dad72c03ecc17a3d887a7da9fcf3ae39a35723c0
changle <mychangle1234@126.com>
2014-06-28 00:41:18 +0900

    added eventbus

--- msg-event-bus/src/main/scala/net/cherry/server/event/EventBusServer.scala
+++ msg-event-bus/src/main/scala/net/cherry/server/event/EventBusServer.scala
@@ -1,12 +1,25 @@
 package net.cherry.server.event
 
-/**
- * Created with IntelliJ IDEA.
- * User: changle
- * Date: 6/24/14
- * Time: 11:17 PM
- * To change this template use File | Settings | File Templates.
- */
-class EventBusServer {
+import com.redis._
+import net.cherry.server.infrastructure.{EventQueueFetcher, EventQueue}
+import akka.actor.{Props, ActorSystem}
+import net.cherry.server.event.application.EventDistributor
 
+object EventBusServer extends App {
+
+  val sourceRedis = new RedisClient("localhost", 6379)
+
+  val targetRedis = new RedisClient("localhost", 6380)
+
+  val sourceEventQueue = EventQueue.ofRedis(sourceRedis)
+
+  val targetEventQueue = EventQueue.ofRedis(targetRedis)
+
+  val system = ActorSystem("event-bus-server")
+
+  val eventDistributor = system.actorOf(Props(new EventDistributor(targetEventQueue)), "event-bus-distributor")
+
+  val eventFetcher = EventQueueFetcher(sourceEventQueue, eventDistributor, system)
+
+  eventFetcher.start
 }
--- msg-event-bus/src/main/scala/net/cherry/server/event/application/EventDistributor.scala
+++ msg-event-bus/src/main/scala/net/cherry/server/event/application/EventDistributor.scala
@@ -1,5 +1,21 @@
 package net.cherry.server.event.application
 
-class EventDistributor {
+import akka.actor.Actor
+import akka.event.Logging
+import net.cherry.domain.model.event.Event
+import net.cherry.server.infrastructure.EventQueue
 
+// TODO: upgrade
+class EventDistributor
+(targetEventQueue: EventQueue)
+  extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case event: Event =>
+      log.info(s"event: $event")
+      targetEventQueue.enqueue(event)
+    case _ =>
+      throw new Exception("unexpected event")
+  }
 }
